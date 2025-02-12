f831bd5e02d3fbb51b7c994bea6eefb906235a14
V2S2P <157969468+V2S2P@users.noreply.github.com>
2025-02-12 20:18:23 +0100

    Added more methods, not have private,sub,leave,bannedwords,getlist and help commands and methods

--- ChatServerDemoMandagUge2/ChatServerDemo.java
+++ ChatServerDemoMandagUge2/ChatServerDemo.java
@@ -32,6 +32,7 @@ public class ChatServerDemo implements IObservable {
                 ClientHandler clientHandler = new ClientHandler(clientSocket,this);
                 clients.add(clientHandler);
                 new Thread(clientHandler).start();
+                broadcast("Welcome to the chat. Please enter #JOIN <username>");
             }
         }catch (IOException e){
             e.printStackTrace();
@@ -50,39 +51,74 @@ public class ChatServerDemo implements IObservable {
         for (ClientHandler clientHandler : clients){
             if (clientHandler.name.equals(username)){
                 clientHandler.notify(filterMessage);
-                break;
+                return;
             }
         }
     }
-    public String filterMessage(String message){
-        for (String word : bannedWords){
-            message = message.replaceAll("(?i)\\b" + word + "\\b", "**");
+    @Override
+    public void sendPrivateMessage(String message, String[] usernames){
+        String filterMessage = filterMessage(message);
+        boolean atLeastOneSent = false;
+
+        for (String username : usernames) {
+            username = username.trim();
+
+            for (ClientHandler clientHandler : clients){
+                if (clientHandler.name != null && clientHandler.name.equals(username)){
+                    clientHandler.notify("Private message: " + filterMessage);
+                    atLeastOneSent = true;
+                }
+            }
+            if (!atLeastOneSent){
+                System.out.println("No users found");
+            }
         }
-        return message;
     }
+    @Override
+    public boolean containsBannedWord(String msg){
+        for (String word : bannedWords){
+            if (msg.toLowerCase().contains(word.toLowerCase())){
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
     public void addBannedWords(String word){
         bannedWords.add(word);
         System.out.println(word + " has been added to the banned word-list");
     }
+    @Override
     public void removeBannedWords(String word){
         bannedWords.remove(word);
         System.out.println(word + " has been removed from the banned word-list");
     }
-    public void getClientList(){
-        String names = null;
-        for (ClientHandler clientHandler : clients){
-
+    @Override
+    public void getClientList(ClientHandler requestingClient) {
+        StringBuilder clientList = new StringBuilder("Connected Clients: ");
+        for (ClientHandler clientHandler : clients) {
+            if (clientHandler.name != null) {
+                clientList.append(clientHandler.name).append(", ");
+            }
         }
+        // Remove last ", " if the list is not empty
+        if (clientList.length() > 18) {  // "Connected Clients: " has 18 characters
+            clientList.setLength(clientList.length() - 2);
+        } else {
+            clientList.append("No clients connected.");
+        }
+        requestingClient.notify(clientList.toString()); // Send list to requesting client
     }
     public void showAllBannedWords(){
 
     }
-    private static class ClientHandler implements Runnable, IObserver {
+    static class ClientHandler implements Runnable, IObserver {
         private IObservable server;
         private Socket clientSocket;
         private PrintWriter out;
         private BufferedReader in;
         private String name = null;
+        private int violations = 0;
         public ClientHandler(Socket socket,IObservable server) throws IOException {
             this.clientSocket = socket;
             this.server = server;
@@ -101,9 +137,16 @@ public class ChatServerDemo implements IObservable {
                         String[] messagePart = message.split(" ", 3);
                         if (messagePart.length < 3){
                             out.println("Invalid private message format.");
+                            continue;
                         }
                         String recipient = messagePart[1];
                         String privateMessage = messagePart[2];
+
+                        if (server.containsBannedWord(privateMessage)){
+                            handleViolations();
+                            continue;
+                        }
+
                         server.sendPrivateMessage("Private message from " + name + ": " + privateMessage, recipient);
                     } else if (message.startsWith("#LEAVE")) {
                         server.broadcast(name + " has left the server");
@@ -126,15 +169,51 @@ public class ChatServerDemo implements IObservable {
                         server.removeBannedWords(word);
                     }else if (message.startsWith("#BANNEDWORDS")) {
                         server.showAllBannedWords();
-                    }else if (message.startsWith("GETLIST")) {
+                    }else if (message.startsWith("#GETLIST")) {
+                        server.getClientList(this);
+                    }else if (message.startsWith("#SUB")) {
+                        String[] messageToSend = message.split(" ", 3);
+                        if (messageToSend.length < 3){
+                            out.println("Invalid Format");
+                            return;
+                        }
+                        String[] toUsers = messageToSend[1].split(",");
+                        String sendMessage = messageToSend[2];
 
+                        server.sendPrivateMessage("From " + name + ": " + sendMessage,toUsers);
+                    }else if (message.startsWith("#HELP")) {
+                        helpCommands();
                     }else {
                         System.out.println(message);
+
+                        if (server.containsBannedWord(message)){
+                            handleViolations();
+                            continue;
+                        }
                         server.broadcast("Message from " + name + ": " + message);
                     }
                 }
             }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        public void helpCommands(){
+            out.println("These are the commands you can use.");
+            out.println("#PRIVATE");
+            out.println("#SUB");
+            out.println("#LEAVE");
+            out.println("#BANNEDWORDS");
+            out.println("GETLIST");
+            out.println("#HELP");
+        }
+        public void handleViolations()throws IOException{
+            violations++;
+            out.println("Warning: You used a banned word! Strike " + violations + "/3");
 
+            if (violations>=3){
+                out.println("You have been banned for using banned words too many times.");
+                server.broadcast(name + " has been banned for violating chat rules.");
+                shutdown();
             }
         }
         public void shutdown() throws IOException {
@@ -146,5 +225,11 @@ public class ChatServerDemo implements IObservable {
         public void notify(String msg){
             out.println(msg);
         }
+    }
+    public String filterMessage(String message){
+        for (String word : bannedWords){
+            message = message.replaceAll("(?i)\\b" + word + "\\b", "**");
+        }
+        return message;
     }
 }
--- ChatServerDemoMandagUge2/IObservable.java
+++ ChatServerDemoMandagUge2/IObservable.java
@@ -1,12 +1,15 @@
 package ChatServerDemoMandagUge2;
 
 import java.io.IOException;
+import ChatServerDemoMandagUge2.ChatServerDemo.*;
 
 public interface IObservable {
     void broadcast(String message);
     void sendPrivateMessage(String message, String username)throws IOException;
+    void sendPrivateMessage(String message, String[] usernames);
     void addBannedWords(String word);
     void removeBannedWords(String word);
     void showAllBannedWords();
-    void getClientList();
+    void getClientList(ClientHandler requestingClient);
+    boolean containsBannedWord(String msg);
 }
